f1b72edb807e772f5af1cafe1f1bed78d23d7e58
ohjmi <ohjmi@naver.com>
2024-03-07 23:34:26 +0900

    - 내용 및 이미지 수정 - reset css 파일 추가

--- parallax.js
+++ parallax.js
@@ -1,4 +1,5 @@
 const title = document.getElementById('title');
+const text = document.getElementById('text');
 const rocket = document.getElementById('rocket');
 const mainPlanet = document.getElementById('mainPlanet');
 const planetLeft = document.getElementById('planetLeft');
@@ -8,12 +9,12 @@ const planetRight = document.getElementById('planetRight');
 window.addEventListener('scroll', () => {
   const value = window.scrollY;
 
-  title.style.marginTop = value * 3 + 'px';
-  rocket.style.marginRight = value * 5 + 'px';
-  rocket.style.marginBottom = value * 2.5 + 'px';
-  mainPlanet.style.marginBottom = -value * 2 + 'px';
+  title.style.marginTop = value * 2 + 'px';
+  text.style.marginTop = value * 2 + 'px';
+  rocket.style.marginRight = value * 1 + 'px';
+  mainPlanet.style.marginBottom = -value * 1 + 'px';
   planetLeft.style.marginLeft = value + 'px';
-  planetLeft.style.marginTop = value * 3 + 'px';
+  planetLeft.style.marginTop = value * 2 + 'px';
   planetRight.style.marginRight = value  + 'px';
-  planetRight.style.marginTop = value * 3 + 'px';
+  planetRight.style.marginTop = value * 2 + 'px';
 })
