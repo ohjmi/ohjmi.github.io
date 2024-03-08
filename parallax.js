c6aeb3d1cd72802e995f071b71cc78a63b26ddb9
ohjmi <ohjmi@naver.com>
2024-03-09 02:06:30 +0900

    - 메인 이미지 변경 및 html 구조 수정 - about me 내용 작성 중...

--- parallax.js
+++ parallax.js
@@ -1,20 +1,22 @@
 const title = document.getElementById('title');
-const text = document.getElementById('text');
 const rocket = document.getElementById('rocket');
 const mainPlanet = document.getElementById('mainPlanet');
 const planetLeft = document.getElementById('planetLeft');
 const planetRight = document.getElementById('planetRight');
+const cloudLeft = document.getElementById('cloudLeft');
+const cloudRight = document.getElementById('cloudRight');
 
 
 window.addEventListener('scroll', () => {
   const value = window.scrollY;
 
   title.style.marginTop = value * 2 + 'px';
-  text.style.marginTop = value * 2 + 'px';
   rocket.style.marginRight = value * 1 + 'px';
   mainPlanet.style.marginBottom = -value * 1 + 'px';
   planetLeft.style.marginLeft = value + 'px';
   planetLeft.style.marginTop = value * 2 + 'px';
   planetRight.style.marginRight = value  + 'px';
   planetRight.style.marginTop = value * 2 + 'px';
+  cloudLeft.style.marginLeft = -value * 2 + 'px';
+  cloudRight.style.marginRight = -value * 2 + 'px';
 })
